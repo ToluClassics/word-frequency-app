37cd8f7e3ba56401dc2c2772ae27dce1fb2a621f
DC_A <ogundepoodunayo@gmail.com>
2020-10-22 13:47:07 +0100

    yes

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "odun omo oba"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_WHITE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
