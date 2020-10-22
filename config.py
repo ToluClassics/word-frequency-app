828f51948e67dc6b4caae5a2ed10363c1f09605c
DC_A <ogundepoodunayo@gmail.com>
2020-10-22 14:28:10 +0100

    edited config file

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "odun omo oba"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
